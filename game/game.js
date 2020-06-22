da8118a0d1762a2d5d03000d5936566d3146ec66
Spencer Cornwall <spencer@slatedev.com>
2020-06-22 14:08:56 -0600

    Fixing resize on soft keyboard (disabling resize)

--- game/game.js
+++ game/game.js
@@ -71,29 +71,28 @@ Game = {
     this.layoutUI('combat', info)
   },
   resize(){
-    Session.set('screenSize', `${$('html').innerWidth()} X ${$('html').innerHeight()}`)
-    var fs = document.fullscreenElement || document.msFullScreenElement
-    var shortSide = $('#Home').innerWidth() < $('#Home').innerHeight() ? 'Width' : 'Height'
-    var grow = ((($('#Home').innerWidth()*9)/$('#Home').innerHeight())/9)*Game.targetHeight
-    if(fs){
-      Game.app.renderer.resize(shortSide == 'Width' ? Game.targetHeight : Math.floor(grow), Game.targetHeight)
-      $('#Home canvas').css({width:'100vw', height:`${$('#Home')[`inner${shortSide}`]()}px`, border:'1px solid white'})
+    if(!Game.focus){
+      Session.set('screenSize', `${$('html').innerWidth()} X ${$('html').innerHeight()}`)
+      var fs = document.fullscreenElement || document.msFullScreenElement
+      var shortSide = $('#Home').innerWidth() < $('#Home').innerHeight() ? 'Width' : 'Height'
+      var grow = ((($('#Home').innerWidth()*9)/$('#Home').innerHeight())/9)*Game.targetHeight
+      if(fs){
+        Game.app.renderer.resize(shortSide == 'Width' ? Game.targetHeight : Math.floor(grow), Game.targetHeight)
+        $('#Home canvas').css({width:'100vw', height:`${$('#Home')[`inner${shortSide}`]()}px`, border:'1px solid white'})
+      }else{
+        Game.app.renderer.resize(Game.targetWidth, Game.targetHeight)
+        $('#Home canvas').css({width:'80%', height:'auto', border:'1px solid black'})
+      }
+      if($('#Home canvas').offset()){
+        $('#UIOverlay').offset({left:$('#Home canvas').offset().left})
+      }
+      $('#UIOverlay').innerWidth(Math.ceil($('#Home canvas').innerWidth()))
+      $('#UIOverlay').innerHeight(Math.ceil($('#Home canvas').innerHeight()))
+
+      for(var spirit of Game.spirits.filter(a=>a.resize)){
+        spirit.resize()
+      }
     }else{
-      Game.app.renderer.resize(Game.targetWidth, Game.targetHeight)
-      $('#Home canvas').css({width:'80%', height:'auto', border:'1px solid black'})
-    }
-    if($('#Home canvas').offset()){
-      $('#UIOverlay').offset({left:$('#Home canvas').offset().left})
-    }
-    $('#UIOverlay').innerWidth(Math.ceil($('#Home canvas').innerWidth()))
-    $('#UIOverlay').innerHeight(Math.ceil($('#Home canvas').innerHeight()))
-
-    for(var spirit of Game.spirits.filter(a=>a.resize)){
-      spirit.resize()
-    }
-
-    if(Game.focus){
-      Game.focus.focus()
       delete Game.focus
     }
   },
